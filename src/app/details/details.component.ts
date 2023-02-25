82dfa82929b47b59257e4f5e4273e8c177272bd6
erikblaha <erik.blaha4@gmail.com>
2023-02-25 17:46:46 +0100

    add order functions, button design, cashout

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { products, IProducts } from '../products';
+import { CartService } from '../cart.service';
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
@@ -10,7 +11,7 @@ export class DetailsComponent implements OnInit {
   product: IProducts = {} as IProducts;
   id: number = 0;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private cs: CartService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
@@ -18,4 +19,18 @@ export class DetailsComponent implements OnInit {
       this.product = products[this.id];
     });
   }
+
+  addToOrder(meals: IProducts){
+    alert("added");
+    this.cs.addToOrder(meals)
+  }
+}
+
+export class MenuComponent {
+  products: Array<IProducts> = products;
+
+  constructor(){
+    
+  }
+
 }
--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -8,10 +8,34 @@ import { IProducts } from '../products';
   styleUrls: ['./order.component.css']
 })
 export class OrderComponent implements OnInit {
-  cart : Array<IProducts> = [];
-  constructor(private cs:CartService){}
+  cart: Array<IProducts> = [];
+
+  constructor(private cs: CartService) { }
 
   ngOnInit(): void {
     this.cart = this.cs.getCart();
+  }
+
+  calcSum(): number {
+    let sum = 0;
+    for (const product of this.cart) {
+      sum += product.price;
+    }
+    return sum;
+  }
+
+  calcService(): number {
+    return this.calcSum() * 0.1;
+  }
+
+  calcDiscount(): number {
+    if (this.calcSum() <= 40) {
+      return 0;
+    }
+    return -this.calcSum() * 0.15;
+  }
+
+  calcTotal(): number {
+    return this.calcSum() + this.calcService() + this.calcDiscount();
   }
 }
